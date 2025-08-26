06c6c3d7665d6c5e2025da674b1278337c5d90d2
Jibril Arbicho <jibrilarbicho2@gmail.com>
2025-08-26 21:47:15 +0300

    rawbody

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from "express";
 import cors from "cors";
 const app = express();
-app.use(express.json());
+app.use(
+  express.json({
+    verify: (req, res, buffer) => {
+      (req as any).rawBody = buffer;
+    },
+  })
+);
 app.use(
   cors({
     origin: "http://localhost:3000",
